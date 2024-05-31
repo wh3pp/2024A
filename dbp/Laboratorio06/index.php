eb96bda310983d39009daebb5826c130310e30b3
wh3pp <remp64041@gmail.com>
2024-05-31 14:17:40 -0500

    DBP - Laboratorio06

---
+++ dbp/Laboratorio06/index.php
@@ -0,0 +1,20 @@
+<?php
+  include("functions.php");
+?>
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styles.css">
+    <title>Tabla Pitagorica</title>
+  </head>
+  <body>
+    <section>
+      <h1>Roni Ezequiel Montañez Pacco</h1>
+      <h2>Tabla Pitagórica</h2>
+      <?php
+        TablaPitagorica();
+      ?>
+    </section>
+  </body>
+</html>
